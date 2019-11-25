81571db22486bff1c37ffa3b925b36c1b6fe7297
unixqnx <unixqnx@mail.ru>
2019-11-26 01:59:59 +0600

    finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import { increment,  decriment} from './actions/index';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter { counter }</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decriment())}>-</button>
+
+      { isLogged ? <h3>Valuable Information I shouldn't see</h3>:null }    
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,53 +5,15 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import allReducers from './reducers/index';
+import { Provider } from 'react-redux';
 
 
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-const store = createStore(allReducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-
-
-/////////////////////////////////////////////////////////////////////////////
-// //STORE -> GLOBALIZED STATE
-
-// //ACTION INCREMENT
-// const increment = () => {
-//     return {
-//         type: 'INCREMENT'
-//     };
-// };
-// const decrement = () => {
-//     return {
-//         type: 'DECREMENT'
-//     };
-// };
-
-
-// //REDUCER
-// const counter = (state = 0, action) => {
-//     switch(action.type){
-//         case 'INCREMENT':
-//             return state + 1;
-//         case 'DECREMENT':
-//             return state - 1;
-//     }
-// };
-// let store = createStore(counter);
-
-// //Display in in the console
-// store.subscribe(() => console.log(store.getState()));
-
-// //DISPATCH
-// store.dispatch(increment());
-// store.dispatch(decrement());
-// store.dispatch(decrement());
-/////////////////////////////////////////////////////////////////////////////
-
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={ store }>
+                    <App />
+                </Provider>, 
+                document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
